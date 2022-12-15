904e617d83e81fa49cb49c2dcc7f02e5e1063997
Miguel <miguezaga2@hotmail.com>
2022-12-15 17:16:44 -0500

    style side mobile

--- src/App.js
+++ src/App.js
@@ -44,25 +44,25 @@ function App() {
           <button className="hero__btn">Read more</button>
         </section>
 
-        <section>
-          <h2>New</h2>
+        <section className="side">
+          <h2 className="side__title">New</h2>
 
-          <article>
-            <h3>Hydrogen VS Electric Cars</h3>
-            <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+          <article className="side__article">
+            <h3 className="side__header">Hydrogen VS Electric Cars</h3>
+            <p className="side__text">Will hydrogen-fueled cars ever catch up to EVs?</p>
           </article>
 
-          <article>
-            <h3>The Downsides of AI Artistry</h3>
-            <p>
+          <article className="side__article">
+            <h3 className="side__header">The Downsides of AI Artistry</h3>
+            <p className="side__text">
               What are the possible adverse effects of on-demand AI image
               generation?
             </p>
           </article>
 
-          <article>
-            <h3>Is VC Funding Drying Up?</h3>
-            <p>
+          <article className="side__article">
+            <h3 className="side__header">Is VC Funding Drying Up?</h3>
+            <p className="side__text">
               Private funding by VC firms is down 50% YOY. We take a look at
               what that means.
             </p>
